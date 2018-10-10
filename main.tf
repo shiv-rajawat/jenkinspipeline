7ba462db0f5349e9a5b7f76b03c1f6e02dcd8779
Shiv Rajawat <shivrajawat@tabahi.com>
2018-10-10 18:09:23 +0530

    Initial commit

---
+++ main.tf
@@ -0,0 +1,36 @@
+###################################
+# @uthor: Shiv Mangal Singh Rajawat
+# Contributor: Anuj Sehgal
+###################################
+provider "aws" {
+  region = "${var.aws_region[0]}"
+
+}
+
+terraform {
+  backend "s3" {
+    
+    encrypt = true
+    bucket = "shiv-terraform-state"
+    dynamodb_table = "terraform-state-lock"
+    key    = "global/s3/terraform.tfstate"
+    region = "us-east-2"
+  }
+}
+
+#####Configuring the provider as AWS.
+
+
+
+##### Creating a VPC in user specified region.
+
+resource "aws_vpc" "main" {
+  count = "${var.vpc_count}"
+  cidr_block           = "${element(var.cidr_prefix, count.index)}.0.0/16"
+  enable_dns_support   = "true"
+  enable_dns_hostnames = "true"
+
+  tags {
+    Name = "${var.vpc_name}${count.index}"
+  }
+}
